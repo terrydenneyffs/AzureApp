5a987a6f8de374eba8e45065ddda2fca4c9c2cd0
Terry Denney <terrydenney@fingerfoodstudios.com>
2018-07-30 15:51:55 -0700

    Fixing nodemon

--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,22 @@ var router = express.Router();
 const title = process.env.TITLE
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  router.get('/', function(req, res, next) {
-    const url = process.env.URL
-    const password = process.env.PASSWORD
-    const user = process.env.USER
-    const dbName = 'library';
+  const url = process.env.URL
+  const password = process.env.PASSWORD
+  const user = process.env.USER
+  const dbName = 'library';
 
-    (async function mongo() {
-      let client;
-      try {
-        client = await MongoClient.connect(url, { auth: { user, password } });
-        const db = client.db(dbName)
-        const response = await db.collection('books').find()
-        res.json(response);
-      } catch (err) {
-        console.log(err)
-      }
-    }())
-  });
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(url, { auth: { user, password } });
+      const db = client.db(dbName)
+      const response = await db.collection('books').find()
+      res.json(response);
+    } catch (err) {
+      console.log(err)
+    }
+  }())
 });
 
 module.exports = router;
